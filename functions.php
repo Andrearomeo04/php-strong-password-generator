186b6806becd19a58ba179105e0ec2158c7a3c46
andrearomeo04 <pisco004@gmail.com>
2024-09-10 10:57:09 +0200

    [UPD] transfered function inside function.php

--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php 
+function randomPassword($length) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
+    $password ='';
+    $charlen = strlen($chars);
 
+    while (strlen($password) < $_GET['length']) {
+        $password .= $chars[rand(0, $charlen - 1)];
+
+    }
+
+    return $password;
+
+}
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php 
 
-function randomPassword($length) {
-    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
-    $password ='';
-    $charlen = strlen($chars);
-
-    while (strlen($password) < $_GET['length']) {
-        $password .= $chars[rand(0, $charlen - 1)];
-
-    }
-
-    return $password;
-
-}
+include './functions.php';
 
 $length = $_GET['length'];
 $password = '';
